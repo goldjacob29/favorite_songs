7cd50ef7eebc882e6aa73913fe53cb29dee9cd77
Raghu Betina <raghu@firstdraft.com>
2017-03-11 03:11:53 +0000

    Direct association between users and playlists

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,9 @@
 class Playlist < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :playlists,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
